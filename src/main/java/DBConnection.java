38c0902079c5348f751d2988fd58ae14c3a9d79f
Miahy03 <hei.miahy.2@gmail.com>
2026-03-10 11:13:26 +0100

    change DBConnection

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,17 +1,37 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private final String dbUrl = System.getenv("DB_URL");
-    private final String dbUser = System.getenv("DB_USER");
-    private final String dbPassword = System.getenv("DB_PASSWORD");
 
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+            Dotenv dotenv = Dotenv.load();
+
+            String jdbcURL = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USERNAME");
+            String password = dotenv.get("DB_PASSWORD");
+
+            return DriverManager.getConnection(jdbcURL, user, password);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+
+        if (connection != null) {
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 }
